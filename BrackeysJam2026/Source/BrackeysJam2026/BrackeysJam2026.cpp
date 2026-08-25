fa3b6752b67af916d74df4e5c14b3652aa56ef3e
Alex <alexander.tran@ymail.com>
2026-08-25 15:12:29 -0700

    UI Progress, added C++ content

---
+++ BrackeysJam2026/Source/BrackeysJam2026/BrackeysJam2026.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BrackeysJam2026.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BrackeysJam2026, "BrackeysJam2026" );

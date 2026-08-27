d84eec7d660472aa85b9c0abe0edd6b990280c35
Calabronx <sebicala@hotmail.com>
2026-08-27 10:37:52 -0300

    remove local folders for a lot of noise in commit files (also conflicts while merging) locally

--- BrackeysJam2026/Intermediate/Build/Win64/BrackeysJam2026Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30159
-#include "E:/_Alex's Data/Unreal Engine/Unreal Projects/Brackeys2026/Brackeys-2026/BrackeysJam2026/Intermediate/Build/Win64/BrackeysJam2026Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
